c4a1e96f4ddff1fb12e7664e9c77f80f8731dcf9
Sam Smith <sam@example.com>
2018-11-23 21:58:25 -0500

    Mejoras

--- clase05_bootstrap/urls.py
+++ clase05_bootstrap/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from backoffice.views import index, listado
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('listado/', listado, name='listado'),
+
 ]
